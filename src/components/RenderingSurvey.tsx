f21db4d2fb8aab8ba657854c1b3395e39a5700b0
dhythm <yuta.okada.20@gmail.com>
2021-10-11 13:01:54 +0900

    survey for re-rendering

--- src/components/RenderingSurvey.tsx
+++ src/components/RenderingSurvey.tsx
@@ -2,15 +2,20 @@ import { useEffect, useState } from "react";
 
 export const RenderingSurvey = () => {
   const [loading, setLoading] = useState(true);
-  let label = "loading...";
+  const [label, setLabel] = useState("loading...");
+  let _label = "loading...";
 
   const onClick = () => {
     setLoading(true);
-    label = "reloading...";
+    setLabel("reloading...");
+    _label = "reloading...";
     console.log(`onClick: ${label}`);
+    // if use setTimeout, re-render will happen twice (setLoading/setLabel and setLoading),
+    // on the other hand, if doesn't use setTimeout, re-render will happen once.
     setTimeout(() => {
       setLoading(false);
     }, 1000);
+    // setLoading(false);
   };
 
   useEffect(() => {
@@ -20,7 +25,8 @@ export const RenderingSurvey = () => {
   }, []);
 
   console.count();
-  console.log(label);
+  console.log({ label });
+  console.log({ _label });
   if (loading) return <div>{label}</div>;
   return (
     <>
