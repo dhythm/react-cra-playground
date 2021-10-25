f8903e1c4d3df05e06537da1dc3a061c81af96e4
dhythm <yuta.okada.20@gmail.com>
2021-10-25 23:24:20 +0900

    wip

--- src/components/Tiptap/NodeView.tsx
+++ src/components/Tiptap/NodeView.tsx
@@ -8,7 +8,7 @@ import {
 } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Resizable } from "re-resizable";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Rnd } from "react-rnd";
 import styled from "styled-components";
 import "./styles.scss";
@@ -18,34 +18,14 @@ const Component = (props) => {
     width: 400,
     height: 200,
   });
-  const [isResizing, setIsResizing] = useState(false);
   const imgRef = useRef<any>(null);
-  //   const increase = () => {
-  //     props.updateAttributes({
-  //       count: props.node.attrs.count + 1,
-  //     });
-  //   };
-
-  //   return (
-  //     <NodeViewWrapper className="react-component">
-  //       <span className="label">React Component</span>
-
-  //       <div className="content">
-  //         <button onClick={increase}>
-  //           This button has been clicked {props.node.attrs.count} times.
-  //         </button>
-  //       </div>
-  //     </NodeViewWrapper>
-  //   );
-  //   useEffect(() => {
-  //     console.log({ ref });
-  //     if (ref?.current) {
-  //       setSize({
-  //         height: ref.current?.height,
-  //         width: ref.current?.width,
-  //       });
-  //     }
-  //   }, [ref]);
+  useEffect(() => {
+    console.log(size.height);
+    console.log({ imgRef: imgRef?.current });
+    console.log(imgRef?.current?.clientHeight);
+    if (imgRef?.current?.height) {
+    }
+  }, [imgRef?.current?.height, size.height]);
 
   return (
     <NodeViewWrapper
@@ -64,8 +44,6 @@ const Component = (props) => {
         size={size}
         bounds="parent"
         enable={{ left: true, right: true }}
-        onResizeStart={() => setIsResizing(true)}
-        onResizeStop={() => setIsResizing(false)}
         onResize={(e, dir, ref) => {
           setSize({
             width: parseInt(ref.style.width, 10),
@@ -77,47 +55,14 @@ const Component = (props) => {
       >
         <img
           ref={imgRef}
+          style={{
+            ...size,
+          }}
           //   src="https://source.unsplash.com/8xznAGy4HcY/800x400"
           src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg"
           alt="sample1"
         />
       </Resizable>
-    </NodeViewWrapper>
-  );
-  return (
-    <NodeViewWrapper
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: size.height,
-        width: "100%",
-        border: "1px solid #ff0000",
-      }}
-    >
-      <StyledRnd
-        size={size}
-        enableResizing={{ left: true, right: true }}
-        onResizeStart={() => setIsResizing(true)}
-        onResizeEnd={() => setIsResizing(false)}
-        onResize={(e, direction, ref, delta, position) => {
-          console.log({ ref });
-          setSize({
-            width: parseInt(ref.style.width, 10),
-            height:
-              (parseInt(ref.style.width, 10) * imgRef.current.naturalHeight) /
-              imgRef.current.naturalWidth,
-          });
-        }}
-        disableDragging
-      >
-        <img
-          ref={imgRef}
-          //   src="https://source.unsplash.com/8xznAGy4HcY/800x400"
-          src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg"
-          alt="sample1"
-        />
-      </StyledRnd>
     </NodeViewWrapper>
   );
 };
