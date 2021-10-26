69ceef843c27ad0d473859ff65b5a81ea3d51c23
dhythm <yuta.okada.20@gmail.com>
2021-10-26 09:19:49 +0900

    feat(tiptap): implement the feature to change image url

--- src/components/Tiptap/NodeView.tsx
+++ src/components/Tiptap/NodeView.tsx
@@ -9,8 +9,6 @@ import {
 import StarterKit from "@tiptap/starter-kit";
 import { Resizable } from "re-resizable";
 import { useRef, useState } from "react";
-import { Rnd } from "react-rnd";
-import styled from "styled-components";
 import "./styles.scss";
 
 const Component = (props) => {
@@ -19,8 +17,8 @@ const Component = (props) => {
     height: 200,
   });
   const imgRef = useRef<any>(null);
+  const imgUrl = props.node.attrs.imgUrl;
 
-  console.log({ ...size });
   return (
     <NodeViewWrapper
       style={{
@@ -53,10 +51,10 @@ const Component = (props) => {
             ...size,
           }}
           //   src="https://source.unsplash.com/8xznAGy4HcY/800x400"
-          src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg"
+          //   src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg"
+          src={imgUrl}
           alt="sample1"
           onLoad={() => {
-            console.log({ img: imgRef.current });
             setSize({
               width: parseInt(imgRef.current.style.width, 10),
               height:
@@ -80,8 +78,8 @@ const ReactComponent = Node.create({
 
   addAttributes() {
     return {
-      count: {
-        default: 0,
+      imgUrl: {
+        default: "",
       },
     };
   },
@@ -104,25 +102,47 @@ const ReactComponent = Node.create({
 });
 
 export const NodeView = () => {
+  const [imgUrl, setImgUrl] = useState(
+    "https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg"
+  );
+
+  const addImage = () => {
+    const url = window.prompt("URL");
+
+    if (url) {
+      //   setImgUrl(url);
+      editor!.commands.setContent(
+        `
+    <p>
+      This is still the text editor you’re used to, but enriched with node views.
+    </p>
+    <react-component imgUrl="${url}"></react-component>
+    <p>
+      Did you see that? That’s a React component. We are really living in the future.
+    </p>
+    `
+      );
+    }
+  };
+
+  console.log({ imgUrl });
   const editor = useEditor({
     extensions: [StarterKit, ReactComponent],
     content: `
     <p>
       This is still the text editor you’re used to, but enriched with node views.
     </p>
-    <react-component count="0"></react-component>
+    <react-component imgUrl="${imgUrl}"></react-component>
     <p>
       Did you see that? That’s a React component. We are really living in the future.
     </p>
     `,
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <div>
+      <button onClick={addImage}>add image from URL</button>
+      <EditorContent editor={editor} />
+    </div>
+  );
 };
-
-const StyledRnd = styled(Rnd)`
-  &:hover {
-    outline: 3px solid #68cef8;
-  }
-  cursor: ew-resize;
-`;
