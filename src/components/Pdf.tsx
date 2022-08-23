c4e04e7688e1d26674e4090f69dce570754e5fda
dhythm <yuta.okada.20@gmail.com>
2022-08-24 02:41:24 +0900

    update

--- src/components/Pdf.tsx
+++ src/components/Pdf.tsx
@@ -1,14 +1,14 @@
-// import PDFViewer from "pdf-viewer-reactjs";
+import PDFViewer from "pdf-viewer-reactjs";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import "./pdf.css";
 
 export const Pdf = () => (
   <>
-    {/* <PDFViewer
+    <PDFViewer
       document={{
         url: "https://arxiv.org/pdf/quant-ph/0410100.pdf",
       }}
-    /> */}
+    />
     <div>
       <Document
         file="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf"
